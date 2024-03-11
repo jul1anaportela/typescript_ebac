455bad20be80cb5a1684f53a5742b7fc1b138674
Juliana Portela <bazil.helpdesk@plan-international.org>
2024-03-10 21:36:42 -0300

    atividade M26 - EBAC

---
+++ atividade/atividade.js
@@ -0,0 +1,8 @@
+"use strict";
+const multiplicaNumero = (numero1, numero2) => numero1 * numero2;
+const saudacao = () => "olá!";
+// function saudacao() {
+//     return "olá!";
+// }
+console.log(saudacao());
+console.log(multiplicaNumero(2, 2));
---
+++ atividade/atividade.ts
@@ -0,0 +1,12 @@
+const multiplicaNumero = (numero1: number, numero2: number): number => numero1 * numero2;
+
+const saudacao = () => "olá!";
+
+
+// function saudacao() {
+//     return "olá!";
+// }
+
+
+console.log(saudacao())
+console.log(multiplicaNumero(2,2))
